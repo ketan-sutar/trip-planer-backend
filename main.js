845265869845d5a09ffb19a273ed7d277d0d4966
ketan-sutar <sutarketan35@gmail.com>
2025-07-06 16:01:45 +0530

    testing2

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ const port = process.env.PORT || 3000;
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
       "https://trip-planer-frontend.vercel.app", // ✅ add this
+      "http://localhost:5173",
     ],
   })
 );
